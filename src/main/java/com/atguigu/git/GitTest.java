667d1e4fc8111e28bb870541997a0207e0114b0a
yumiao <1365562173@qq.com>
2022-07-14 17:56:51 +0800

    hot-fix第一次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
         System.out.println("Hello Git5");
+        System.out.println("===============================");
+        System.out.println("hot-fix");
     }
 }
