ccca05af1fbe22d57b0fcf6d5ac62f88e3912b77
yumiao <1365562173@qq.com>
2022-07-14 17:51:44 +0800

    idea种git第二次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
