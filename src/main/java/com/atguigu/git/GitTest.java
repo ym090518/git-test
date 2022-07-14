8e96941bc0cc7b5fa6f142b38ec3fe26e500ee5e
yumiao <1365562173@qq.com>
2022-07-14 18:11:54 +0800

    推送到远程库

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("=================合并冲突=================");
         System.out.println("hot-fix分支修改");
         System.out.println("master修改");
+        System.out.println("=================分享到远程库===================");
+        System.out.println("VCS---->Import into Version Control---->Share Project on Github");
     }
 }
